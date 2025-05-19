90b51d8f6dae6bc432de43f7b982c11058c9a481
Rafael <seijiarakaki12@gmail.com>
2025-05-18 22:18:38 -0300

    estilizando o header de sobre,quiz,carros

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -11,22 +11,29 @@ document.addEventListener('DOMContentLoaded',()=>{
 
    
     const questoes=[
-        "qual seu modelo de carro favorito",
-        "Que características você mais valoriza em um carro?",
-        "Se você pudesse ter qualquer carro do mundo, qual escolheria?",
-        "Qual foi o melhor carro que você já dirigiu?",
-        "O que mais chamou sua atenção em nossa loja?",
-        "Que cor de carro você mais gosta e por quê?",
-        "Você prefere carros automáticos ou manuais? Explique sua escolha",
+        "1)qual seu modelo de carro favorito",
+        "2)Que características você mais valoriza em um carro?",
+        "3)Se você pudesse ter qualquer carro do mundo, qual escolheria?",
+        "4)Qual foi o melhor carro que você já dirigiu?",
+        "5)O que mais chamou sua atenção em nossa loja?",
+        "6)Que cor de carro você mais gosta e por quê?",
+        "7)Você prefere carros automáticos ou manuais?",
+        "8)O que é mais importante para você: desempenho, economia de combustível ou design?",
+        "9)Você se considera mais apaixonado por carros clássicos ou modernos?",
+        "10)Você daria prioridade a um carro elétrico hoje em dia? "
+        
+
     ]
    
     let perguntas = 0;
     const respostas = [];
 
     function atualizarBarraProgresso() {
-    const progresso = (perguntas / questoes.length) * 100;
+    const progresso = (perguntas / questoes.length) * 111;
     barraProgresso.style.width = `${progresso}%`;
+    
 }
+
 function mostrarPergunta(){
     if(perguntas < questoes.length){
         pergunta.textContent = questoes[perguntas];
@@ -36,6 +43,7 @@ function mostrarPergunta(){
     }else{
         mostrarResultado();
     }
+
 }
      function mostrarResultado(){
         containerPerguntas.classList.add('hidden');
